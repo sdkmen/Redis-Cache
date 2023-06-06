e67c63ee28dfdc21bcb581a18312ee1769a1d3ee
Seyfullah Dikmen <seyfullah_dikmen@hotmail.com>
2023-06-06 14:58:27 +0300

    Work with Pub/Sub

---
+++ Redis.PubSub.Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("message: ");
+    string message = Console.ReadLine();
+
+    long clientNumber = await subscriber.PublishAsync("mychannel", message);
+
+    //using pattern-matching
+    //long clientNumber = await subscriber.PublishAsync("mychannel.x", message);
+    Console.WriteLine(clientNumber + " clients received the message");
+}
---
+++ Redis.PubSub.Subscriber/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+//using pattern-matching subscription
+
+//await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+//{
+//    Console.WriteLine(message);
+//});
+
+Console.Read();
